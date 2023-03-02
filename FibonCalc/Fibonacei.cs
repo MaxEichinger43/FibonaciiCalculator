900ae846450608f93ec030d9edeb3581c675429f
MaxEichinger43 <126464027+MaxEichinger43@users.noreply.github.com>
2023-03-02 13:28:53 +0100

    Add files via upload

---
+++ FibonCalc/Fibonacei.cs
@@ -0,0 +1,31 @@
+using System;
+
+
+namespace FibonCalc
+{
+        class Fibonacei
+        {
+            static void Main(string[] args)
+            {
+
+                List<int> Fibonaceifolge = new List<int>();
+                Fibonaceifolge.Add(1);
+                Fibonaceifolge.Add(1);
+                
+
+                Console.WriteLine("Hallo! Welche Stelle der Fibonacei Folge möchtest du wissen?");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+
+                for (int i = 2; i == (n); i++ )
+                {
+                    Fibonaceifolge.Add(Fibonaceifolge[0] + Fibonaceifolge[1]);
+                }
+
+
+                Console.WriteLine(Fibonaceifolge[n-1]);
+                
+                Console.ReadKey();
+            }
+        }
+}
