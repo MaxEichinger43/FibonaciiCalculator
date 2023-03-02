b1d9cae68243fb01d53c766af6d6a28c46937870
Max Eichinger <mx.eichinger@gmx.de>
2023-03-02 14:05:59 +0100

    Versuche hinzugefügt

--- FibonCalc/Fibonacei.cs
+++ FibonCalc/Fibonacei.cs
@@ -7,25 +7,41 @@ namespace FibonCalc
         {
             static void Main(string[] args)
             {
+                Console.WriteLine("Wie viele Versuche möchtest du?");
+
+                int a = Convert.ToInt32(Console.ReadLine());
+
+
+                for (int i = 0; i < a; i++)
+                {
+                    Program();
+                }
+
+
+                Console.WriteLine("Alle Versuche verbraucht!");
+                Console.ReadKey();
+            }
+
+            static void Program()
+            {
 
                 List<int> Fibonaceifolge = new List<int>();
                 Fibonaceifolge.Add(1);
                 Fibonaceifolge.Add(1);
                 
 
-                Console.WriteLine("Hallo! Welche Stelle der Fibonacei Folge möchtest du wissen?");
+                Console.WriteLine("\nHallo! Welche Stelle der Fibonacei Folge möchtest du wissen?");
                 int n = Convert.ToInt32(Console.ReadLine());
 
 
-                for (int i = 2; i == (n+1); i++ )
+                for (int i = 2; i == (n + 1); i++)
                 {
-                    Fibonaceifolge.Add(Fibonaceifolge[0] + Fibonaceifolge[1]);
+                    Fibonaceifolge.Add(Fibonaceifolge[i-1] + Fibonaceifolge[i-2]);
                 }
 
 
-                Console.WriteLine(Fibonaceifolge[n-1]);
+                Console.WriteLine("Die " + n + ". Stelle ist eine " + Fibonaceifolge[n - 1]);
                 
-                Console.ReadKey();
             }
         }
 }
