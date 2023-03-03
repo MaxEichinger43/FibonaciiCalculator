0e46357ac09c13fcac241390d60c9076bd285382
Max Eichinger <mx.eichinger@gmx.de>
2023-03-03 12:04:43 +0100

    Added english and cleaner code

--- Fibonacei.cs
+++ Fibonacei.cs
@@ -1,83 +1,107 @@
 using System;
 
-namespace FibonCalc
+namespace FibonaceiCalculator
 {
-        class Fibonacei
+    class Fibonacei
+    {
+
+
+        static void Main(string[] args)
         {
+            Console.Title = "Fibonacei Calculator";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("This is a calculator for numbers of the Fibonacei-Row.");
+            Console.WriteLine("\n\nYou can choose as many tickets as you wish Fibonacei numbers to now.");
+            Console.WriteLine("\n------------Press any key to continue------------");
+            Console.ForegroundColor = ConsoleColor.White;
 
-//Programmstart
-            static void Main(string[] args)
+            Console.ReadKey();
+
+            int inputcount;
+            bool optionalOutput;
+
+
+            InputOptions(out inputcount, out optionalOutput);
+
+            for (int i = 0; i < inputcount; i++)
             {
-                Console.Title = "Fibonacei Calculator";
-
-
-//Input Optionen
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Wie viele Versuche möchtest du haben?  (int)");
-                Console.ForegroundColor = ConsoleColor.White;
-                int a = Convert.ToInt32(Console.ReadLine());
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Sollen die Schritte bis zu Gesuchten auch angezeigt werden?  (true/false)");
-                Console.ForegroundColor = ConsoleColor.White;
-                bool optionalOutput = Convert.ToBoolean(Console.ReadLine());
-                Console.WriteLine("So sei es!");
-
-
-//Calculator Ansteuerung
-                for (int i = 0; i < a; i++)
-                {
-                    Calculator(optionalOutput);
-                }
-
-
-//Ende des Programms
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Alle Versuche verbraucht!");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.ReadKey();
+                Calculator(optionalOutput);
             }
 
-
-//Calculator
-            static void Calculator(bool optionalOutput)
-            {
-
-
-
-//Input
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\nWelche Stelle der Fibonacei Folge möchtest du wissen?   (int)");
-                Console.ForegroundColor = ConsoleColor.White;
-                int n = Convert.ToInt32(Console.ReadLine());
-
-
-
-//Listengeneration & Erweiterung basierend auf Input(*)
-                List<int> Fibonaceifolge = new List<int>();
-                Fibonaceifolge.Add(1);
-                Fibonaceifolge.Add(1);                
-                int a = 2;
-                while (a < n)//(*)
-                {
-                    Fibonaceifolge.Add(Fibonaceifolge[a-1] + Fibonaceifolge[a-2]);
-                    a++;
-                }
-
-
-
-//Falls alle Stellen bis zur gefragten Eingegebenwerden ausgegeben werden sollen
-                if (optionalOutput == true)
-                {
-                    Console.WriteLine("\n.......");for (int i = 1; i < Fibonaceifolge.Count; i++){Console.WriteLine(Fibonaceifolge[i-1]);}
-                }
-
-
-//Finale Ausgabe                
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Die " + n + ". Stelle ist eine:"); Console.ForegroundColor = ConsoleColor.Green; Console.Write(Fibonaceifolge[n - 1]);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\n-----------------------------------------------------");
-            }
+            OutOfTickets();
         }
+
+        //Reads input parameters
+        private static void InputOptions(out int inputcount, out bool optionalOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nHow many tickets do you want?");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            inputcount = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Do you also want to now all the steps until your asked Number?  (true/false)");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            optionalOutput = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("So it is!");
+        }
+
+        //Takes Input
+        private static int Input()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nWhat number of the Fibonacei-Row do you wish to now?");
+            Console.ForegroundColor = ConsoleColor.White;
+            int targetslot = Convert.ToInt32(Console.ReadLine());
+            return targetslot;
+        }
+
+        //Caluculates Fibonacei numbers
+        static void Calculator(bool optionalOutput)
+        {
+            int targetslot = Input();
+
+            List<int> Fibonaceifolge = new List<int>();
+            Fibonaceifolge.Add(1);
+            Fibonaceifolge.Add(1);
+            int currentslot = 2;
+            while (currentslot < targetslot)
+            {
+                Fibonaceifolge.Add(Fibonaceifolge[currentslot - 1] + Fibonaceifolge[currentslot - 2]);
+                currentslot++;
+            }
+
+            Output(optionalOutput, targetslot, Fibonaceifolge);
+        }
+
+        //Outputs the calculatet Result
+        private static void Output(bool optionalOutput, int targetslot, List<int> Fibonaceifolge)
+        {
+            if (optionalOutput == true)
+            {
+                Console.WriteLine("\n......."); for (int i = 1; i < Fibonaceifolge.Count; i++) { Console.WriteLine(Fibonaceifolge[i - 1]); }
+            }
+
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("The " + targetslot + ". number is:"); Console.ForegroundColor = ConsoleColor.Green; Console.Write(Fibonaceifolge[targetslot - 1]);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\n-----------------------------------------------------");
+        }
+
+        //Gives information about the ticketcount
+        private static void OutOfTickets()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("No tickets left!");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ReadKey();
+        }
+    }
 }
